c1bb27243ae11417719d7d5ff5ed624084b97100
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-05-13 21:03:13 +0200

    reviews

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,18 @@
+const ReviewCard = ({ data }) => {
+
+    const { text, vote, name } = data;
+
+    return (
+        <>
+            <div className="card mt-4">
+                <div className="card-body">
+                    <p>{text}</p>
+                    <p>Vote: {vote}</p>
+                    <p>By: {name}</p>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default ReviewCard;
--- src/pages/MoviesDetail.jsx
+++ src/pages/MoviesDetail.jsx
@@ -1,10 +1,56 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ReviewCard from "../components/ReviewCard";
+
 const MoviesDetailPage = () => {
 
 
+    //leggere l'id che arriva in pagina
+    const { id } = useParams();
+
+    const [movie, setMovie] = useState({});
+
+    function getMovie() {
+        axios(`http://localhost:3000/api/movies/${id}`)
+            .then(response => {
+                setMovie(response.data)
+            }
+            )
+            .catch(err => console.log(err))
+    }
+
+    useEffect(getMovie, []);
+
+    function renderReviews() {
+        return movie.reviews.map(review => <ReviewCard key={review.id} data={review} />)
+    }
+
+
     return (
-        <div>
-            pagina del dettaglio del film
-        </div>
+        <>
+            <div className="container row">
+                <div className="col-6">
+                    <h1>{movie.title}</h1>
+                    <img className="w-50 mt-2" src={movie.imagePath} alt={movie.title} />
+                </div>
+                <div className="col-6 d-flex flex-column justify-content-center">
+                    <p>year: <strong>{movie.release_year}</strong></p>
+                    <p>genre: <strong>{movie.genre}</strong></p>
+                    <p>director: <strong>{movie.director}</strong></p>
+                    <p>{movie.abstract}</p>
+                </div>
+            </div>
+            <section id="review" className="container mt-4">
+                <header className="d-flex justify-content-between align-items-center">
+                    <h4>Community reviews</h4>
+                    <div>Average vote: {movie.reviews_vote}</div>
+                </header>
+                {movie.reviews?.length ?
+                    renderReviews()
+                    : <div>nessuna recensione</div>}
+            </section>
+        </>
     )
 };
 
